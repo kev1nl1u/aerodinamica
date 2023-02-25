96d357a4e1b6f8165134b5c539f29eec6f4ec3a1
Kevin Liu <kevliujudi@gmail.com>
2023-02-25 16:22:09 +0100

    Update

--- js/moveonscroll.js
+++ js/moveonscroll.js
@@ -1,6 +1,5 @@
-var windowHeight = window.innerHeight;
 var windowWidth = window.innerWidth;
-var scrollArea = windowHeight;
+var scrollArea = window.innerHeight;
 var plane = document.getElementsByClassName('main-title-img')[0];
 
 var left = plane.offsetLeft;
@@ -10,4 +9,12 @@ window.addEventListener('scroll', function() {
 	
 	plane.style.left = left + scrollPercent*window.innerWidth + 'px';
 
+	if(plane.offsetRight >= vw2px(100)){
+		plane.style.display = "none";
+	}
 });
+
+
+function vw2px(vw){
+	return vw * window.innerWidth / 100;
+}
